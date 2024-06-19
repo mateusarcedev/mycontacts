97460cbea248d8fdc5e741e3f2e5b6d87b0ac390
Mateus Arce <arcemateuss@gmail.com>
2024-06-19 19:14:25 -0400

    first commit

---
+++ api/src/app/controllers/ContactController.js
@@ -0,0 +1,79 @@
+const ContactsRepository = require("../repositories/ContactsRepository");
+
+class ContactController {
+  async index(request, response) {
+    const contacts = await ContactsRepository.findAll()
+
+    response.json(contacts)
+  }
+
+  async show(request, response) {
+    const { id } = request.params
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      response.status(404).json({ error: 'Usuário não encontrado' })
+    }
+
+    return response.json(contact)
+  }
+
+  async store(request, response) {
+    const { name, email, phone, category_id } = request.body
+
+    if (!name) {
+      return response.status(400).json({ error: 'Nome é obrigatório' })
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email)
+    if (!contactExists) {
+      return response.status(400).json({ error: 'O email já está sendo utilizado' })
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id
+    })
+
+    return response.json(contact)
+  }
+
+  async update(request, response) {
+    const { id } = request.params
+    const { name, email, phone, category_id } = request.body
+
+    const contactExists = await ContactsRepository.findById(id)
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Usuário não encontrado' })
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Nome é obrigatório' })
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email)
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'O email já está sendo utilizado' })
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id
+    })
+
+    return response.json(contact)
+  }
+
+  async delete(request, response) {
+    const { id } = request.params
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      response.status(404).json({ error: 'Usuário não encontrado' })
+    }
+
+    await ContactsRepository.delete(id)
+    return response.sendStatus(204)
+  }
+
+}
+
+module.exports = new ContactController();
